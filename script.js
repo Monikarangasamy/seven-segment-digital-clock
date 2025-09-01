1a7676dfdfabf63783be60a0606b2be21d0a8019
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-09-01 20:57:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function changeTime() {
     let hour = String(now.getHours()).padStart(2, '0');
     let minute = String(now.getMinutes()).padStart(2, '0');
     let second = String(now.getSeconds()).padStart(2, '0');
+    let ampm = now.getHours() >= 12 ? ['p', 'm'] : ['a', 'm'];
 
     document.getElementById("hour-1").setAttribute("class","num-"+hour[0]);
     document.getElementById("hour-2").setAttribute("class","num-"+hour[1]);
@@ -10,16 +11,10 @@ function changeTime() {
     document.getElementById("minute-2").setAttribute("class","num-"+minute[1]);
     document.getElementById("second-1").setAttribute("class","num-"+second[0]);
     document.getElementById("second-2").setAttribute("class","num-"+second[1]);
+    document.getElementById("ampm-1").setAttribute("class","num-"+ampm[0]);
+    document.getElementById("ampm-2").setAttribute("class","num-"+ampm[1]);
 
     setTimeout(changeTime, 1000);
 }
 
 changeTime();
-
-
-
-  
-
-
-
-
