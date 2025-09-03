a70838fc5e5af60b0b8091efa6a3842b261e3a7d
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-09-03 19:59:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+function formatDate(d) {
+  const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  return `${days[d.getDay()]}, ${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+}
+
+
 function changeTime() {
     let now = new Date();
     let hour = String(now.getHours()).padStart(2, '0');
@@ -5,7 +13,8 @@ function changeTime() {
     let second = String(now.getSeconds()).padStart(2, '0');
     let ampm = now.getHours() >= 12 ? "PM" : "AM";
 
-    // update digit segments
+
+    document.getElementById("date").innerText = formatDate(now);
     document.getElementById("hour-1").setAttribute("class","num-"+hour[0]);
     document.getElementById("hour-2").setAttribute("class","num-"+hour[1]);
     document.getElementById("minute-1").setAttribute("class","num-"+minute[0]);
@@ -13,9 +22,10 @@ function changeTime() {
     document.getElementById("second-1").setAttribute("class","num-"+second[0]);
     document.getElementById("second-2").setAttribute("class","num-"+second[1]);
 
-    // update AM/PM text
+
     document.getElementById("ampm").innerText = ampm;
 
     setTimeout(changeTime, 1000);
 }
 changeTime();
+
