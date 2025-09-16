45b19123198e7b0004a0bf3d8420666c57ece073
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-09-16 14:12:19 +0530

    Add files via upload

--- level_1/script.js
+++ level_1/script.js
@@ -1,50 +1,46 @@
+class SevenSegment {
+  constructor(elementId) {
+    this.element = document.getElementById(elementId);
+    }
 
-function formatDate(d) {
-  const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-  return `${days[d.getDay()]}, ${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+  setDigit(num) {
+    this.element.textContent = num;
+    }
 }
 
-
-function changeTime() {
-    let now = new Date();
-    let hour = String(now.getHours()).padStart(2, '0');
-    let minute = String(now.getMinutes()).padStart(2, '0');
-    let second = String(now.getSeconds()).padStart(2, '0');
-    let ampm = now.getHours() >= 12 ? "PM" : "AM";
-
-
-    document.getElementById("date").innerText = formatDate(now);
-    document.getElementById("hour-1").setAttribute("class","num-"+hour[0]);
-    document.getElementById("hour-2").setAttribute("class","num-"+hour[1]);
-    document.getElementById("minute-1").setAttribute("class","num-"+minute[0]);
-    document.getElementById("minute-2").setAttribute("class","num-"+minute[1]);
-    document.getElementById("second-1").setAttribute("class","num-"+second[0]);
-    document.getElementById("second-2").setAttribute("class","num-"+second[1]);
-
-
-    document.getElementById("ampm").innerText = ampm;
-
-    setTimeout(changeTime, 1000);
-}
-changeTime();
-
-function updateGreeting() {
-  const now = new Date();
-  let hour = now.getHours();
-  let greeting;
-
-  if (hour < 12) {
-    greeting = "Good Morning";
-  } else if (hour < 18) {
-    greeting = "Good Afternoon";
-  } else {
-    greeting = "Good Evening";
-  }
-
-  document.getElementById("name").innerText = greeting;
+class DigitalClock {
+  constructor(hour1, hour2, min1, min2, sec1, sec2) {
+    this.hour1 = new SevenSegment(hour1);
+    this.hour2 = new SevenSegment(hour2);
+    this.min1 = new SevenSegment(min1);
+    this.min2 = new SevenSegment(min2);
+    this.sec1 = new SevenSegment(sec1);
+    this.sec2 = new SevenSegment(sec2);
 }
 
-updateGreeting();
-setInterval(updateGreeting, 60 * 60 * 1000);
+  updateTime() {
+    const now = new Date();
+    const hour = String(now.getHours()).padStart(2, '0');
+    const minute = String(now.getMinutes()).padStart(2, '0');
+    const second = String(now.getSeconds()).padStart(2, '0');
+
+    this.hour1.setDigit(hour[0]);
+    this.hour2.setDigit(hour[1]);
+    this.min1.setDigit(minute[0]);
+    this.min2.setDigit(minute[1]);
+    this.sec1.setDigit(second[0]);
+    this.sec2.setDigit(second[1]);
+}
+
+start() {
+  const tick = () => {
+    this.updateTime();
+      setTimeout(tick, 1000);
+    };
+      tick();
+    }
+}
+
+const clock = new DigitalClock("hour-1", "hour-2", "minute-1", "minute-2", "second-1", "second-2");
+clock.start();
 
